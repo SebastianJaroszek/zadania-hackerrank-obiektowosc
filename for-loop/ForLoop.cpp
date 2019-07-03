bd54b4265504f8e2bd32e3c78ed5a7fe38fc42aa
Sebastian Jaroszek <s.jaroszek@wspa.edu.pl>
2019-07-03 19:01:03 +0200

    Rozwiązanie zadania For Loop.

--- for-loop/ForLoop.cpp
+++ for-loop/ForLoop.cpp
@@ -2,8 +2,24 @@
 #include <cstdio>
 using namespace std;
 
+string number_names[] = {
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+};
+
 int main() {
-    // Complete the code.
+    int a, b;
+    scanf("%i\n%i", &a, &b);
+
+    for (int i = a; i <= b; i++) {
+        if (i <= 9) {
+            cout << number_names[i - 1] << endl;
+        } else if (i > 9 && i % 2 == 0) {
+            cout << "even" << endl;
+        } else if (i > 9 && i % 2 != 0) {
+            cout << "odd" << endl;
+        }
+    }
+
     return 0;
 }
 
