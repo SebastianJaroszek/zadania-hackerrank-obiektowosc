1ed3da31f8f22832051f452a6cf021c82debbe9d
Sebastian Jaroszek <s.jaroszek@wspa.edu.pl>
2019-07-03 21:07:10 +0200

    Rozwiązanie zadania Variable Sized Arrays.

--- variable-sized-arrays/VariableSizedArrays.cpp
+++ variable-sized-arrays/VariableSizedArrays.cpp
@@ -5,9 +5,34 @@
 #include <algorithm>
 using namespace std;
 
+vector<int> createArray(int arrayLength) {
+    vector<int> arr;
+
+    for (int i = 0; i < arrayLength; i++) {
+        int number;
+        scanf("%i", &number);
+        arr.push_back(number);
+    }
+    return arr;
+}
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int numberOfArrays, numberOfQueries;
+    scanf("%i %i", &numberOfArrays, &numberOfQueries);
+    vector<vector<int>> arrayOfArrays;
+    for (int i = 0; i < numberOfArrays; i++) {
+        int arrayLength;
+        scanf("%i", &arrayLength);
+        vector<int> arr = createArray(arrayLength);
+        arrayOfArrays.push_back(arr);
+    }
+
+    for (int i = 0; i < numberOfQueries; i++) {
+        int x, y;
+        scanf("%i %i", &x, &y);
+        cout << arrayOfArrays[x][y] << endl;
+    }
+
     return 0;
 }
 
