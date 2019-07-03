7954d01b7edbe7cdf8dc291f0530a8121627c115
Sebastian Jaroszek <s.jaroszek@wspa.edu.pl>
2019-07-03 19:48:57 +0200

    Rozwiązanie zadania Arrays Introduction.

--- arrays-introduction/ArraysIntroduction.cpp
+++ arrays-introduction/ArraysIntroduction.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    scanf("%i", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        int number;
+        scanf("%i", &number);
+        arr[i] = number;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[n - 1 - i] << " ";
+    }
+
     return 0;
 }
