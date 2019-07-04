02aa71375bc60550869dc4e8ab3b7bd1bfeae77a
Sebastian Jaroszek <s.jaroszek@wspa.edu.pl>
2019-07-04 19:36:58 +0200

    Rozwiązanie zadania Strings.

--- strings/Strings.cpp
+++ strings/Strings.cpp
@@ -3,8 +3,18 @@
 using namespace std;
 
 int main() {
-	// Complete the program
+    string a, b;
   
+    cin >> a >> b;
+
+    cout << a.size() << " " << b.size() << endl;
+
+    string concatenated = a + b;
+    cout << concatenated << endl;
+
+    a[0] = b[0];
+    b[0] = concatenated[0];
+    cout << a << " " << b << endl;
     return 0;
 }
 
