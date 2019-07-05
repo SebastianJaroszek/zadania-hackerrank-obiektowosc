2f35f39e491f2cfcb42ae4617aec28bd31686df4
Sebastian Jaroszek <s.jaroszek@wspa.edu.pl>
2019-07-05 20:49:16 +0200

    Rozwiązanie zadania Virtual Functions.

--- virtual-functions/VirtualFunctions.cpp
+++ virtual-functions/VirtualFunctions.cpp
@@ -4,6 +4,91 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+#include <sstream>
+
+class Person {
+    public:
+        string name;
+        int age;
+    
+    public:
+        virtual void getdata();
+        virtual void putdata();
+};
+
+void Person::getdata() {
+    cin >> name >> age;
+}
+
+void Person::putdata() {
+    stringstream result;
+    result << name << " ";
+    result << age << " ";
+    cout << result.str();
+}
+
+class Professor : public Person {
+    public:
+        int publications;
+        int cur_id;
+        void getdata();
+        void putdata();
+        static int id;
+};
+int Professor::id = 0;
+
+class Student : public Person {
+    public:
+        vector<int> marks;
+        int cur_id;
+        void getdata();
+        void putdata();
+        int calculateMarks();
+        static int id;
+};
+int Student::id = 0;
+
+void Professor::getdata() {
+    Person::getdata();
+    cin >> publications;
+    id = id + 1;
+    cur_id = id;
+}
+
+void Professor::putdata() {
+    Person::putdata();
+    stringstream result;
+    result << publications << " ";
+    result << cur_id;
+    cout << result.str() << endl;
+}
+
+void Student::getdata() {
+    Person::getdata();
+    for (int i = 0; i < 6; i++) {
+        int mark;
+        cin >> mark;
+        marks.push_back(mark);
+    }
+    id = id + 1;
+    cur_id = id;
+}
+
+void Student::putdata() {
+    Person::putdata();
+    stringstream result;
+    result << calculateMarks() << " ";
+    result << cur_id;
+    cout << result.str() << endl;
+}
+
+int Student::calculateMarks() {
+    int result = 0;
+    for (int i = 0; i < marks.size(); i++) {
+        result = result + marks[i];
+    }
+    return result;
+}
 
 int main(){
 
